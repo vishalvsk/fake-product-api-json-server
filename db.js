daae39099b202abd29cbf67a4b3e4c8aa9121cbf
vishal Karale <vishalkaralevsk@gmail.com>
2022-09-04 17:58:47 +0530

    Done

--- db.js
+++ db.js
@@ -1,13 +1,8 @@
-const items = require("./data/items");
-const tags = require("./data/tags");
-const brands = require("./data/brands");
-const itemTypes = require("./data/itemTypes");
+const Car = require("./data/Car.json");
+
 
 module.exports = {
-  items,
-  tags,
-  brands,
-  itemTypes
+ Car
 }
 
 
